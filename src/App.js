50957845a5c31aeb37882d1dc46ec51cbcf2a47b
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2022-09-25 16:03:27 -0700

    initial implementation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
 import Calendar from "@ericz1803/react-google-calendar";
 import creds from './calendar-config.json';
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import React from 'react';
 
 const API_KEY = creds.apiKey;
-const calendars = [
-  { calendarId: "" }
-];
+
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:calendarId" element={<CalendarEmbed />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const CalendarEmbed = () => {
+  const { calendarId } = useParams();
+  console.log(calendarId);
+  const calendars = [
+    { calendarId: calendarId }
+  ];
   return (
     <div>
       <Calendar apiKey={API_KEY} calendars={calendars} />
     </div>
-  );
+  )
 }
 
 export default App;
